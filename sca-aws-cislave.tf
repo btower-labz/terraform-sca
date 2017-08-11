0095090b725890fd3aca7ee90a9b612487ca91d6
BTowerLabz <dev@btower.net>
2017-08-11 20:45:36 +0000

    sec docswarm

--- sca-aws-cislave.tf
+++ sca-aws-cislave.tf
@@ -9,7 +9,45 @@ resource "aws_security_group" "sca_cislave_sec" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.sca_admin_ip}"]
+    cidr_blocks = [
+      "${var.sca_admin_ip}"
+    ]
+  }
+
+  ingress {
+    from_port = 2377
+    to_port = 2377 
+    protocol = "tcp"
+    cidr_blocks = [
+      "${var.sca_world_ip}"
+    ]
+  }
+
+  ingress {
+    from_port = 7946
+    to_port = 7946
+    protocol = "tcp"
+    cidr_blocks = [
+      "${var.sca_world_ip}"
+    ]
+  }
+
+  ingress {
+    from_port = 7946
+    to_port = 7946
+    protocol = "udp"
+    cidr_blocks = [
+      "${var.sca_world_ip}"
+    ]
+  }
+
+  ingress {
+    from_port = 4789 
+    to_port = 4789
+    protocol = "udp"
+    cidr_blocks = [
+      "${var.sca_world_ip}"
+    ]
   }
 
   egress {
